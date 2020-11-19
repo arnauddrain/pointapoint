1f2d01e69e69334b6143c0bf6540f4b195f1a340
Arnaud Drain <drain.arnaud@hotmail.fr>
2020-11-20 00:09:45 +0100

    Lint save function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,24 +54,19 @@ export class AppComponent {
 
   save() {
     const svg = document.getElementById('canvas');
-    var serializer = new XMLSerializer();
-    var source = serializer.serializeToString(svg);
+    const serializer = new XMLSerializer();
+    let source = serializer.serializeToString(svg);
 
-    //add name spaces.
     if (!source.match(/^<svg[^>]+xmlns="http\:\/\/www\.w3\.org\/2000\/svg"/)) {
       source = source.replace(/^<svg/, '<svg xmlns="http://www.w3.org/2000/svg"');
     }
     if (!source.match(/^<svg[^>]+"http\:\/\/www\.w3\.org\/1999\/xlink"/)) {
       source = source.replace(/^<svg/, '<svg xmlns:xlink="http://www.w3.org/1999/xlink"');
     }
-
-    //add xml declaration
     source = '<?xml version="1.0" standalone="no"?>\r\n' + source;
 
-    //convert svg source to URI data scheme.
-    var url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
+    const url = "data:image/svg+xml;charset=utf-8," + encodeURIComponent(source);
 
-    //set url value to a element's href attribute.
     (document.getElementById("link") as HTMLLinkElement).href = url;
     (document.getElementById("link") as HTMLLinkElement).click();
   }
